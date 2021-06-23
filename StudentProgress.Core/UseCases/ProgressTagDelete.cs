fc7b290039f675a0189b610a6e269cb41ee71043
Alexander van Dam <a.vandam@fontys.nl>
2021-06-23 12:08:38 +0200

    first version of progress tag delete use case.

---
+++ StudentProgress.Core/UseCases/ProgressTagDelete.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using StudentProgress.Core.Entities;
+
+namespace StudentProgress.Core.UseCases
+{
+    public class ProgressTagDelete
+    {
+        private readonly ProgressContext _db;
+
+        public ProgressTagDelete(ProgressContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Result> HandleAsync(Command command)
+        {
+            var progressTag = await _db.ProgressTags.FindAsync(command.Id);
+
+            _db.ProgressTags.Remove(progressTag);
+
+            await _db.SaveChangesAsync();
+            return Result.Success();
+        }
+
+        public record Command
+        {
+            public int Id { get; set; }
+        }
+    }
+}
